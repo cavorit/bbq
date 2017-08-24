ad5b97ca7e1928572b9f760d2d20b5e6d6ce7cb1
HF(at)bbq <teilnehmer@kinshasabeatboy.kinshasabeatboy.de>
2017-08-24 10:04:20 +0200

    Regex Hausaufgaben

--- re_sandbox/regex.py
+++ re_sandbox/regex.py
@@ -1,6 +1,25 @@
-sep = "-"
-valD = r"(\w?0?[1-9]|[12][\d]|3[01])" + sep
-valM = r"(\w?0?[1-9]|1[0-2])" + sep
-valY = r"[12]\d{3}"
+import re
 
-regex = valD + valM + valY 
+richtig = ["24-12-2017", "31-12-2017", "03-11-1983"]
+falsch = ["Jana", "24-12:2017", "24-13-2017", "00-12-2011", "32-10-1999",
+        "01-01-22112", "3-0-1998", "1-1-2019"  ]
+
+myRegex =  r'((0[1-9])|([12][0-9])|(3[01]))-((0[1-9])|(1[0-2]))-([0-9]{4})'
+
+
+
+
+def date_tester(pattern, text):
+    
+    for txt in text:
+        m = re.search(pattern, txt)
+        if m:
+            print('%s is a match' % (txt))
+        else:
+            print('%s is NOT a match' % (txt))
+
+if __name__ == "__main__":
+    date_tester(pattern = myRegex, text=richtig)
+    date_tester(pattern = myRegex, text=falsch)
+
+
