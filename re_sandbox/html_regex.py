e7160581bad7334ce356f37e1a0d925203066642
HF bei BBQ <harald.fiedler@cavorit.de>
2017-08-25 13:35:53 +0200

    Einbinden von URL begonnen

--- re_sandbox/html_regex.py
+++ re_sandbox/html_regex.py
@@ -1,6 +1,11 @@
 import re
+import urllib.request
 
 Zeilen = ['# Eine Zeile mit einem Kommentar ', '<a href="http://www.taz.de">Beschreibung</a>', '<a href="http://www.tagesschau.de">Die Tagesschau</a>', '<a href="https://cavorit.de">Cavorit</a>'] 
+
+#Webseite = "http://cavorit.de"
+#Zeilen = urllib.request.urlopen(Webseite)
+ 
 
 # print("Name: {0}, Link: {1},.format(m.group(2), m.group(1)))
 pattern = r"((?<=<a href=[\"\'])(?P<Link>.+)[\'\"]>(?P<Beschreibung>.+)(?=</a>))"
@@ -14,5 +19,4 @@ for Zeile in Zeilen:
         print("z%s: %s (%s)" % (z, m.group('Beschreibung'), m.group('Link')))
     else:
         print("z%s: --- no entry found" %z)
-
 
