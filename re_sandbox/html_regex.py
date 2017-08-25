e064483e64cd4ba65271aa7e48b2479d189d7789
HF bei BBQ <harald.fiedler@cavorit.de>
2017-08-25 13:21:42 +0200

    Regex Übung 2 hinzegefuegt: finde URL auf Internetseite. Es muss noch implementiert werden das tatsaechliche zeilenweise scannen von Internetseiten.

---
+++ re_sandbox/html_regex.py
@@ -0,0 +1,18 @@
+import re
+
+Zeilen = ['# Eine Zeile mit einem Kommentar ', '<a href="http://www.taz.de">Beschreibung</a>', '<a href="http://www.tagesschau.de">Die Tagesschau</a>', '<a href="https://cavorit.de">Cavorit</a>'] 
+
+# print("Name: {0}, Link: {1},.format(m.group(2), m.group(1)))
+pattern = r"((?<=<a href=[\"\'])(?P<Link>.+)[\'\"]>(?P<Beschreibung>.+)(?=</a>))"
+
+z = 0
+for Zeile in Zeilen:
+    m = re.search(pattern, string = Zeile)
+    z+=1 
+    if m:
+        #print("Name")#: {0}, Link: {1}".format(m.group(Name), m.group(Link)))
+        print("z%s: %s (%s)" % (z, m.group('Beschreibung'), m.group('Link')))
+    else:
+        print("z%s: --- no entry found" %z)
+
+
