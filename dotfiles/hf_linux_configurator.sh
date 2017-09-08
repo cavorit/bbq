b9821e7157171fd6a3122ca05dd27cbcb45b6b80
hf <harald.fiedler@cavorit.de>
2017-09-08 20:04:41 +0200

    Überarbeitung meiner linux conf

--- dotfiles/hf_linux_configurator.sh
+++ dotfiles/hf_linux_configurator.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
-    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
+apt-get install tmux, tree
 
-sudo apt-get remove vim vim-runtime gvim
-sudo apt-get remove vim-tiny vim-common vim-gui-common vim-nox
+apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev
+apt get libgtk2.0-dev libatk1.0-dev libbonoboui2-dev
+apt-get libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev
+apt-get python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
+
+apt-get remove vim vim-runtime gvim
+apt-get remove vim-tiny vim-common vim-gui-common vim-nox
 
 cd ~
 git clone https://github.com/vim/vim.git
@@ -26,16 +28,16 @@ cd vim
 make VIMRUNTIMEDIR=/usr/local/share/vim/vim80
 
 cd ~/vim
-sudo make install
+make install
 
-sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
-sudo update-alternatives --set editor /usr/bin/vim
-sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
-sudo update-alternatives --set vi /usr/bin/vim
+update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
+update-alternatives --set editor /usr/bin/vim
+update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
+update-alternatives --set vi /usr/bin/vim
 
-vim --version
 cp vimrc.default ~/.vimrc
-cp tmux.conf.default ~/.tmux.conf  
+cp tmux.conf.default ~/.tmux.conf
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "Bitte vim starten mit test.py und :PluginInstall ausführen"
+echo "vim wurde für YouCompleteMe mit /usr/lib/python3.5 kompiliert"
