369664c161b2f6d7aa45b0713cad737e133b9ef6
Harald Fiedler <harald.fiedler@cavorit.de>
2017-09-14 18:36:07 +0200

    minor changes

--- dotfiles/ubuntu_configurator.sh
+++ dotfiles/ubuntu_configurator.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 apt install tmux, tree
-
 apt install libncurses5-dev libgnome2-dev libgnomeui-dev
 apt install libgtk2.0-dev libatk1.0-dev libbonoboui2-dev
 apt install libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev
