e225fbbb58ab4b00b5c48e37b9b3c166d940fc26
hfiedler <harald.fiedler@cavorit.de>
2017-09-18 16:17:55 +0200

    db homework

--- db_playground/db.py
+++ db_playground/db.py
@@ -6,14 +6,14 @@ dbconfig = { 'host' : '127.0.0.1',
              'database' : 'nameDB'}
 
 conn = mysql.connector.connect(**dbconfig)
-cursor = conn.cursor()
+c = conn.cursor()
 
 
 # Cavorit Convention: SQL-Abfragen in _SQL speichern.
 
-cursor.execute("""DROP TABLE IF EXISTS lager""")
-cursor.execute("""DROP TABLE IF EXISTS lieferanten""")
-cursor.execute("""DROP TABLE IF EXISTS kunden""")
+c.execute("""DROP TABLE IF EXISTS lager""")
+c.execute("""DROP TABLE IF EXISTS lieferanten""")
+c.execute("""DROP TABLE IF EXISTS kunden""")
 
 
 _SQL = """CREATE TABLE lager(
@@ -24,7 +24,7 @@ _SQL = """CREATE TABLE lager(
           reserviert INTEGER)
        """   
  
-cursor.execute(_SQL)
+c.execute(_SQL)
 
 _SQL = """CREATE TABLE lieferanten(
           kurzname TEXT,
@@ -32,7 +32,7 @@ _SQL = """CREATE TABLE lieferanten(
           telefonnummer TEXT)
        """   
 
-cursor.execute(_SQL)
+c.execute(_SQL)
 
 _SQL = """CREATE TABLE kunden(
           kundennummer INTEGER,
@@ -40,46 +40,31 @@ _SQL = """CREATE TABLE kunden(
           anschrift TEXT)
        """
 
-cursor.execute(_SQL)
+c.execute(_SQL)
 
 _SQL = """INSERT INTO lager VALUES(
           1, 2607198, 'Grafikkarte Typ A', 'FC' , 0
           )"""
-          
-cursor.execute(_SQL)
+print("eins")        
+c.execute(_SQL)
 conn.commit()
-
-print("------------------ #1")
+print("zwei")
 
 werte = (1, 26071987, 'Grafikkarte Typ 1', 'SCHADCODE666', 0)
-
-# So ist ein Sicherheitsrisiko, weil python die Zuordnung vornimmt, was
-# malcode-injection ermöglicht
-# _SQL = """INSERT INTO lager VALUES ({0}, {1}, '{2}', '{3}', {4})""".format(*werte)
-#
-# print(_SQL)
-
-# Sicher ist, wenn man das DB-Management-System die Zuordnung vornehmen lässt
-
-
-_SQL = """INSERT INTO lager VALUES (?,?,?,?,?)"""
-print("----------------- #2")
-print(_SQL)
-print(werte)
-print("-----------------")
-cursor.execute(_SQL, werte)
-conn.commit()
+_SQL = """INSERT INTO lager VALUES (%s,%s,%s,%s,%s)""" 
+c.execute(_SQL, werte) # this way no SQL-injection possible.
+# security issue: cursor.execute("""INSERT INTO lager VALUES ({0}{1}{2}{3}{4})""".format(...
 
 
 print("Ich bin jetzt da")
 
-#werte = ((1, "2607871987", "Grafikkarte Typ 1", "FC", 0),
-#(2, "19870109", "Prozessor Typ 13", "LPE", 57),
-#(10, "06198823", "Netzteil Typ 3", "FC", 0),
-#(25, "11198703", "LED-Lüfter", "FC", 57),
-#(26, "19880105", "Festplatte 128 GB", "LPE", 12))
+werte = ((1, 260787, "Grafikkarte Typ 1", "FC", 0),
+(2, 19809, "Prozessor Typ 13", "LPE", 57),
+(10, 6123, "Netzteil Typ 3", "FC", 0),
+(25, 118703, "LED-Lüfter", "FC", 57),
+(26, 19105, "Festplatte 128 GB", "LPE", 12))
 
-#_SQL = """INSERT INTO lager VALUES (?,?,?,?,?)"""
+_SQL = """INSERT INTO lager VALUES (%s, %s, %s, %s, %s)"""
 
-#cursor.executemany(_SQL, werte)
-
+c.executemany(_SQL, werte)
+conn.commit()
