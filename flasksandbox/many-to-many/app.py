72f642731d770b147cf17b6a3e8b868d0d0ec15c
hfiedler <harald.fiedler@cavorit.de>
2018-03-02 16:46:30 +0100

    documenting flask n2n

--- flasksandbox/many-to-many/app.py
+++ flasksandbox/many-to-many/app.py
@@ -14,9 +14,27 @@ association_table = db.Table('p2h',
 class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
-    owner = db.relationship('Human', secondary='p2h', backref=db.backref('my_ownder', lazy='dynamic'))
+    owner = db.relationship('Human', secondary='p2h', backref=db.backref('my_owner', lazy='dynamic'))
     
 
 class Human(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
+
+
+
+db.create_all()
+m = Pet(name='Molly')
+s = Pet(name='Sosstaine')
+h = Human(name='Harald')
+r = Human(name='Familie Richter')
+db.session.add(m)
+db.session.add(s)
+db.session.add(h)
+m.owner.append(h)
+m.owner.append(r)
+s.owner.append(h)
+db.session.commit()
+
+
+
