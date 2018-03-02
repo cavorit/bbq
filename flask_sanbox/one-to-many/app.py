4dc3b3c8b0151135ade60ae47d25bd2fbb5d93ba
CGoalStatistics HF <cdo@cgoal.de>
2018-03-02 20:55:49 +0100

    minor changes

--- flask_sanbox/one-to-many/app.py
+++ flask_sanbox/one-to-many/app.py
@@ -6,6 +6,8 @@ db  = SQLAlchemy(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
+# Pet <--n:1--> Human
+
 class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
@@ -26,15 +28,30 @@ class Human(db.Model):
 # > python3
 # >>>> from app import *
 # >>>> db.create_all()
-# >>>> harald = Human(name='Harald Fiedler')
-# >>>> molly = Pet(name='Molly')
-# >>>> waldi = Pet(name='Waldi')
-# >>>> molly.owner = harald
-# >>>> waldi.owner = harald
-# >>>> db.session.add(harald)
+# >>>> h = Human(name='Harald Fiedler')
+# >>>> m = Pet(name='Molly')
+# >>>> s= Pet(name='Sosten')
+# >>>> m.owner = harald
+# >>>> s.owner = harald
+# >>>> db.session.add(m)
+# >>>> db.session.add(s)
+# >>>> db.session.add(h)
+# >>>> m.owner = h
+# >>>> s.owner = h
 # >>>> db.session.commit()
 # >>>> exit()
 # > sqlite3 db.sqlite
 # >>> .table
 # >>> select * from human;
+
+# select * from human;
+#id|name
+#1| Harald
+
+# select * from pet;
+#id|name|owner_id
+#1|molly|1
+#2|sosten|1
+
+
 
